1105c370e184d632d0fcf95c984cb4855be2c3fd
Adylsha Yumayev <adylsha.yumayev@gelecekvarlik.com.tr>
2025-05-29 11:20:00 +0300

    Basket microservice and redis docker added.

---
+++ CourseMicroservice.Basket.API/Program.cs
@@ -0,0 +1,18 @@
+using CourseMicroservice.Basket.API;
+using CourseMicroservice.Shared.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCommonServiceExtension(typeof(BasketAssembly));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.Run();
