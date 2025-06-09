1f681fe1dc9fad42a640fb76924a946c9766f39e
AdylshaY <adylshay@gmail.com>
2025-06-09 20:56:32 +0300

    Order Microservice entity configurations added.

---
+++ CourseMicroservice.Order.Persistence/Configurations/AddressConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using CourseMicroservice.Order.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CourseMicroservice.Order.Persistence.Configurations
+{
+    public class AddressConfiguration : IEntityTypeConfiguration<Address>
+    {
+        public void Configure(EntityTypeBuilder<Address> builder)
+        {
+            builder.HasKey(a => a.Id);
+            builder.Property(a => a.Id).UseIdentityColumn();
+            builder.Property(a => a.Province).HasMaxLength(50).IsRequired();
+            builder.Property(a => a.District).HasMaxLength(50).IsRequired();
+            builder.Property(a => a.Line).HasMaxLength(200).IsRequired();
+            builder.Property(a => a.ZipCode).HasMaxLength(20).IsRequired();
+        }
+    }
+}
