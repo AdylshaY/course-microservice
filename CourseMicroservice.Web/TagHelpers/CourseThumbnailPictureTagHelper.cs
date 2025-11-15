c962d97f1c04ecc33bfe885f3248d118d1b9db1b
AdylshaY <adylshay@gmail.com>
2025-11-16 00:24:56 +0300

    Add course thumbnail support and file microservice config

---
+++ CourseMicroservice.Web/TagHelpers/CourseThumbnailPictureTagHelper.cs
@@ -0,0 +1,32 @@
+﻿using CourseMicroservice.Web.Options;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace CourseMicroservice.Web.TagHelpers
+{
+    public class CourseThumbnailPictureTagHelper(MicroserviceOption microserviceOption) : TagHelper
+    {
+        public string? Src { get; set; }
+
+        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "img";
+
+            var blankCourseThumbnailImagePath = "/images/default-course-thumbnail.jpg";
+
+            if (string.IsNullOrEmpty(Src))
+            {
+                output.Attributes.SetAttribute("src", blankCourseThumbnailImagePath);
+            }
+            else
+            {
+                var courseThumbnailImagePath = $"{microserviceOption.File.BaseAddress}/{Src}";
+
+
+                output.Attributes.SetAttribute("src", courseThumbnailImagePath);
+            }
+
+
+            return base.ProcessAsync(context, output);
+        }
+    }
+}
